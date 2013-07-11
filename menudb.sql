8ceea92f13de7246069fe67e6badc5be36b9e2b9
Dmitry Legchikov <cool@dmitry.by>
2013-07-11 23:55:19 +0400

    Update menudb.sql

--- menudb.sql
+++ menudb.sql
@@ -31,7 +31,6 @@ USE `menudb`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `categoryName` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
   KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
